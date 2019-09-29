5bf98f33e2dc45cfe57e77b20082ccac4dc41f62
Ardi Ismail <>
2019-09-29 10:35:42 +0700

    adding unit test repeat func

--- repeat_test.go
+++ repeat_test.go
@@ -1,41 +1,12 @@
 package iteration
 
-import (
-	"fmt"
-	"testing"
-)
-
-func assertCorrectMessage(t *testing.T, expected, repeated string) {
-	t.Helper()
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
-}
+import "testing"
 
 func TestRepeat(t *testing.T) {
+	got := Repeated("a")
+	want := "aaaaa"
 
-	t.Run("repeated 'a' 5 times if zero inputed", func(t *testing.T) {
-		repeated := Repeat("a", 5)
-		expected := "aaaaa"
-		assertCorrectMessage(t, expected, repeated)
-	})
-
-	t.Run("repeated 'a' 7 times ", func(t *testing.T) {
-		repeated := Repeat("a", 7)
-		expected := "aaaaaaa"
-		assertCorrectMessage(t, expected, repeated)
-	})
-
-}
-
-func BenchmarkRepeat(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Repeat("a", 0)
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
-}
-
-func ExampleRepeat() {
-	char := Repeat("b", 8)
-	fmt.Println(char)
-	// Output: bbbbbbbb
 }
