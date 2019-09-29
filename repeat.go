3a36c0da5c1941bb71b47fc78ed1ec24b2076352
Ardi Ismail <>
2019-09-29 10:45:35 +0700

    refactor the for-loop

--- repeat.go
+++ repeat.go
@@ -1,11 +1,13 @@
 package iteration
 
+const repeatCount = 5
+
 // Repeated only return character
 func Repeated(character string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
-		repeated = repeated + "a"
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
 }
